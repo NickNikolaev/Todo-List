f3cc2910d395ee399cbeedae4f09c891249295ca
NikolaNikolaev <nikola_creators@abv.bg>
2022-01-03 16:28:31 +0200

    Initialize components (Todo, TodoForm and TodoList)
    Add new folder "components" and 3 files inside it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoForm from "./components/TodoForm";
 
 function App() {
     return (
-        <div className="App">
+        <div className="todo-app">
             <h1>App</h1>
+            <TodoForm />
         </div>
     );
 }
